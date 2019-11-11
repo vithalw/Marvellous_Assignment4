823ef13c43f8d429eaddf65e48cafbd4f5d65024
Vithal Waghmode <47021242+vithalw@users.noreply.github.com>
2019-11-11 12:41:58 +0530

    Add files via upload

---
+++ 4_4.py
@@ -0,0 +1,17 @@
+from functools import reduce
+
+def main():
+
+    arr=[int(x) for x in input("enter space saperated numeric value").split()]
+
+    evenArr = list(filter(lambda no:  no%2==0, arr))
+    print(evenArr)
+
+    ModArray = list(map(lambda no: no**2, evenArr))
+    print(ModArray)
+    sum = reduce(lambda a, b: a + b, ModArray)
+    print(sum)
+
+
+if __name__=='__main__':
+    main();
